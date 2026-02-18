8cc16d202a03a1979111d1229676d46a5c6e7c06
StarAtNyte <khanalnitij20@gmail.com>
2026-02-18 17:02:07 +0545

    Works

--- fix-nginx-api-port.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's|proxy_pass http://localhost:8080/api/;|proxy_pass http://localhost:8810/api/;|' /etc/nginx/includes/gpu-polling.conf
-nginx -t && nginx -s reload && echo "Done"
